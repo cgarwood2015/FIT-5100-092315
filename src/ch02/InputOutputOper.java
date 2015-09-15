37a536bec86d85bc94fcaf5bfdf94f387397afe2
Clayton <Clayton@STATION-1>
2015-09-15 00:10:01 -0400

    1224

---
+++ src/ch02/InputOutputOper.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ch02;
+
+/**
+ * @author Clayton Omar. Garwood
+ *
+ */
+public class InputOutputOper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArithmeticOper calArith = new ArithmeticOper();
+		calArith.printArithmetics();
+
+	}
+	
+
+}
